e96c1c714cf6be2b0f07079b7858ca10784ba6d1
Yury Yuchkavich <Yury_Yuchkavich@epam.com>
2019-02-21 16:34:25 +0300

    test

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  
+    var count = 0;
+    for (var i = 0; i< preferences.length ; i++){
+        
+        var a = preferences[i]-1;
+        var b = preferences[a]-1;
+        var c =preferences[b] -1;
+        if(preferences[i] == preferences[c]) {count ++;}
+        preferences[i] = 0;
+    }
+    return count;
+    
+    // your implementation
 };
